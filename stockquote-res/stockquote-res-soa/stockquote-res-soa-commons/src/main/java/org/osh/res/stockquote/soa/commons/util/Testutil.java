ceb535e88301995c0020e2704b87b82fa7215f72
Octavio Sanchez Huerta <osanchezhuerta@gmail.com>
2017-04-10 12:24:30 -0500

    creation of stock princeton classes

---
+++ stockquote-res/stockquote-res-soa/stockquote-res-soa-commons/src/main/java/org/osh/res/stockquote/soa/commons/util/Testutil.java
@@ -0,0 +1,11 @@
+package org.osh.res.stockquote.soa.commons.util;
+
+import org.springframework.util.StringUtils;
+
+public class Testutil {
+
+	public static void main(String[] args) {
+      StringUtils.containsWhitespace(" ");
+	}
+
+}
